f051b2b831a4f7663aa813196e8126e9fdd323ab
Zuhair Malik <zuhairm2001@hotmail.com>
2025-02-06 22:40:17 +1100

    intialised angular view, created contact api service

---
+++ contactListApp/contact-app-view/src/app/services/contact.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Contact {
+  id?: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ContactService {
+
+  private apiUrl = 'https://localhost:5228/api/Contacts';
+  
+  constructor(private http: HttpClient ) { }
+
+  getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(this.apiUrl);
+  }
+
+  addContact(contact: Contact): Observable<Contact> {
+    return this.http.post<Contact>(this.apiUrl, contact);
+  }
+}
--- contactListApp/obj/Debug/net8.0/contactListApp.AssemblyInfo.cs
+++ contactListApp/obj/Debug/net8.0/contactListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("contactListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5eeced7062d31cc0655fc7957d286d26613b959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c816c67ef50cdbfe987a4509c90fc15d1650344")]
 [assembly: System.Reflection.AssemblyProductAttribute("contactListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("contactListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
